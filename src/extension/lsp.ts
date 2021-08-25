0537e2943615c18a937dcef58573a171b050be00
Charlie Moog <moogcharlie@gmail.com>
2021-08-25 15:38:45 -0500

    check if sqls is in $PATH

--- src/extension/lsp.ts
+++ src/extension/lsp.ts
@@ -5,6 +5,7 @@ import {
 } from 'vscode-languageclient/node';
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as child from 'child_process';
 
 export interface LspConfig {
   host: string;
@@ -19,21 +20,30 @@ export type SqlsDriver = 'mysql' | 'postgresql' | 'mysql8' | 'sqlight3'; // TODO
 
 export const binDir = path.join(__filename, '..', '..', '..', 'bin');
 
+const sqlsInPath = (): boolean => {
+  try {
+    return child.spawnSync('which', ['sqls']).status === 0;
+  } catch {
+    return false;
+  }
+};
+
 export class SqlLspClient {
   private client: LanguageClient | null;
   constructor() {
     this.client = null;
   }
   start(config: LspConfig) {
+    if (!sqlsInPath()) {
+      return;
+    }
     let serverOptions: ServerOptions = {
-      command: path.join(binDir, 'sqls-linux-amd64'),
+      command: "sqls",
       args: [],
     };
 
     let clientOptions: LanguageClientOptions = {
-      documentSelector: [
-        { language: 'sql', },
-      ],
+      documentSelector: [{ language: 'sql' }],
       initializationOptions: {
         connectionConfig: {
           driver: config.driver,
