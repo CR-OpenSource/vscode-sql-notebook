3e9f9e001bc31ab424d03738185eb56d1aa03608
chris rollings <chris.rollings@d55.co.uk>
2022-03-28 08:25:53 +0100

    create custom serializer

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,10 @@ import { connectToDatabase, deleteConnectionConfiguration } from './commands';
 import { Pool, ExecutionResult, TabularResult, Row } from './driver';
 import { activateFormProvider } from './form';
 import { SqlLspClient } from './lsp';
+const stringify = require('json-stringify-safe');
 
 const notebookType = 'sql-notebook';
+const customNotebookType = 'sqln-notebook';
 export const storageKey = 'sqlnotebook-connections';
 
 export const globalConnPool: { pool: Pool | null } = {
@@ -20,6 +22,12 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.workspace.registerNotebookSerializer(
       notebookType,
       new SQLSerializer()
+    )
+  );
+  context.subscriptions.push(
+    vscode.workspace.registerNotebookSerializer(
+      customNotebookType,
+      new CustomSQLSerializer()
     )
   );
   const connectionsSidepanel = new SQLNotebookConnections(context);
@@ -46,7 +54,110 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
-export function deactivate() {}
+export function deactivate() { }
+
+interface RawNotebookCell {
+  language: string;
+  value: string;
+  kind: vscode.NotebookCellKind;
+  editable?: boolean;
+  outputs: RawCellOutput[];
+}
+
+interface RawCellOutput {
+  mime: string;
+  value: any;
+}
+
+class CustomSQLSerializer implements vscode.NotebookSerializer {
+  async deserializeNotebook(
+    context: Uint8Array,
+    _token: vscode.CancellationToken
+  ): Promise<vscode.NotebookData> {
+    var contents = new TextDecoder().decode(context);    // convert to String to make JSON object
+
+    // Read file contents
+    let raw: RawNotebookCell[];
+    try {
+      raw = <RawNotebookCell[]>JSON.parse(contents);
+    } catch {
+      raw = [];
+    }
+
+    function convertRawOutputToBytes(raw: RawNotebookCell) {
+      let result: vscode.NotebookCellOutputItem[] = [];
+
+      for (let output of raw.outputs) {
+        let data = new TextEncoder().encode(stringify(output.value));
+        result.push(new vscode.NotebookCellOutputItem(data, output.mime));
+      }
+
+      return result;
+    }
+
+    // Create array of Notebook cells for the VS Code API from file contents
+    const cells = raw.map(item => new vscode.NotebookCellData(
+      item.kind,
+      item.value,
+      item.language
+    ));
+
+    for (let i = 0; i < cells.length; i++) {
+      let cell = cells[i];
+      cell.outputs = raw[i].outputs ? [new vscode.NotebookCellOutput(convertRawOutputToBytes(raw[i]))] : [];
+    }
+
+    // Pass read and formatted Notebook Data to VS Code to display Notebook with saved cells
+    return new vscode.NotebookData(
+      cells
+    );
+
+  }
+
+  async serializeNotebook(
+    data: vscode.NotebookData,
+    _token: vscode.CancellationToken
+  ): Promise<Uint8Array> {
+    // function to take output renderer data to a format to save to the file
+    function asRawOutput(cell: vscode.NotebookCellData): RawCellOutput[] {
+      let result: RawCellOutput[] = [];
+      for (let output of cell.outputs ?? []) {
+        for (let item of output.items) {
+          let outputContents = '';
+          try {
+            outputContents = new TextDecoder().decode(item.data);
+          } catch {
+
+          }
+
+          try {
+            let outputData = JSON.parse(outputContents);
+            result.push({ mime: item.mime, value: outputData });
+          } catch {
+            result.push({ mime: item.mime, value: outputContents });
+          }
+        }
+      }
+      return result;
+    }
+
+    // Map the Notebook data into the format we want to save the Notebook data as
+
+    let contents: RawNotebookCell[] = [];
+
+    for (const cell of data.cells) {
+      contents.push({
+        kind: cell.kind,
+        language: cell.languageId,
+        value: cell.value,
+        outputs: asRawOutput(cell)
+      });
+    }
+
+    // Give a string of all the data to save and VS Code will handle the rest 
+    return new TextEncoder().encode(stringify(contents));
+  }
+}
 
 class SQLSerializer implements vscode.NotebookSerializer {
   async deserializeNotebook(
